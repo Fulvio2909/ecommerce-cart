d946ee5b6846a66d25622e6c9aedb75f26cb42cc
Fulvio2909 <fulvio29904@gmail.com>
2024-03-04 11:10:30 +0100

    04/03/2024

--- cart/cart.js
+++ cart/cart.js
@@ -1,64 +1,30 @@
-/**
- * 1) Permettere di inserire nel carrello dei prodotti "Add to cart".
- * Dichiarazione della funzione
- * input: prodotto
- */
-const addToCart = (aCart, prod) => {
-    aCart.push(prod);
+export const addToCart = (aCart, aProd) => {
+    aCart.push(aProd);
     return aCart;
 }
 
-/**
- * 2) Aggiungiamo 3 prodotti differenti nel carrello
- * input: il carrello
- * output: carrello con i nuovi 3 prodotti
- */
 export const add3Products = (aCart) => {
-    let newCart = addToCart(aCart, {
-        name: 'Iphone',
-        price: 1500,
-        qty: 2,
-    });
-
-    newCart = addToCart(aCart, {
-        name: 'Galaxy S24',
-        price: 1500,
-        qty: 1,
-    });
-
-    newCart = addToCart(aCart, {
-        name: 'Pixel 8',
-        price: 1500,
-        qty: 1,
-    });
-
+    let newCart = addToCart(aCart, {name: 'iPhone', price: 1500, qty: 2});
+    newCart = addToCart(aCart, {name: 'Galaxy S24', price: 1500, qty: 1});
+    newCart = addToCart(aCart, {name: 'Pixel 8', price: 1500, qty: 1});
     return newCart;
 };
 
-/**
- * 3) Calcolo del totale da pagare. "Get amount"
- * input: il carrello
- * output: totale da pagare
- */
-export const getTotalAmount = (aCart) => {
-    return aCart.reduce((total, product) => total + product.price * product.qty, 0);
+export const getAmount = (cart) => {
+    let amount = cart.reduce((accumulatore, numero)=>{
+        return accumulatore + numero.price * numero.qty;
+    },0);
+    return amount;
 };
 
-/**
- * 4) Calcolo del totale quantità prodotti. "Get total qty"
- * input: il carrello
- * output: totale quantità prodotti
- */
-export const getTotalQuantity = (aCart) => {
-    return aCart.reduce((totalQty, product) => totalQty + product.qty, 0);
+export const getTotalQty = (cart) => {
+    let quantity = cart.reduce((accumulatore, numero)=>{
+        return accumulatore + numero.qty;
+    },0);
+    return quantity;
 };
 
-/**
- * 5) Funzionalità che svuota il carrello "Reset cart"
- * input: il carrello
- * output: carrello vuoto
- */
-export const resetCart = () => {
-    return [];
-};
-
+export const resetCart = (cart) =>{
+    cart.splice(0,cart.length);
+    return cart;
+}
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { add3Products, getTotalAmount, getTotalQuantity, resetCart  } from './cart/cart.js'
+import { add3Products, getAmount, getTotalQty, resetCart } from "./cart/cart.js";
 
 let cart = []; //carrello globale
 
@@ -10,21 +10,14 @@ let cart = []; //carrello globale
  * 5) FUnzionalità che svuota il carrello "Reset cart"
  */
 
-// 1) Permettere di inserire nel carrello dei prodotti "Add to cart".
-cart = add3Products(cart);
-
-// 2) Aggiungiamo 3 prodotti differenti nel carrello.
-console.log( add3Products (cart));
-
-// 3) Calcolo del totale da pagare. "Get amount"
-console.log('Totale da pagare:', getTotalAmount(cart));
-
-// 4) Calcolo del totale quantità prodotti nel carrello. "Get total qty"
-console.log('Totale quantità prodotti nel carrello:', getTotalQuantity(cart));
-
-// 5) Funzionalità che svuota il carrello "Reset cart"
-cart = resetCart();
-console.log('Nuovo stato del carrello:', cart);
+//aggiungi tre elementi
+console.log(add3Products(cart));
+//calcolo prezzo totale
+console.log(getAmount(cart));
+//calcolo numero dei prodotti
+console.log(getTotalQty(cart));
+//svuota l'array
+console.log(resetCart(cart));
 
 
 
